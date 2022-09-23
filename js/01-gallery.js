9923e06bfff34c30ad954d2a1efa1e01922fadd5
Oleh Savaryn <olegsavaryn@gmail.com>
2022-09-23 18:36:38 +0200

    #1 complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,43 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryContainer = document.querySelector(".gallery");
 
-console.log(galleryItems);
+const renderedMurkup = render(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", renderedMurkup);
+
+galleryContainer.addEventListener("click", openModal);
+
+function render(element) {
+  return element
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+     data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+    })
+    .join("");
+}
+
+function openModal(event) {
+  event.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+  instance.show();
+
+  window.addEventListener("keydown", escape);
+  function escape(event) {
+    event.preventDefault();
+    if (event.code !== "Escape") {
+      return;
+    }
+    instance.close();
+  }
+}
