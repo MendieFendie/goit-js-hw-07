13ada2a5b42fca8076ad8f6d2b07c9571f911ab6
Oleh Savaryn <olegsavaryn@gmail.com>
2022-09-23 23:20:14 +0200

    #2 complete

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+
+const renderedMurkup = render(galleryItems);
+
+galleryContainer.insertAdjacentHTML("beforeend", renderedMurkup);
+
+galleryContainer.addEventListener("click", openModal);
+
+function render(element) {
+  return element
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+const lightbox = new SimpleLightbox(".gallery a");
+lightbox.defaultOptions.captionsData = "alt";
+lightbox.defaultOptions.captionDelay = "300";
+lightbox.on("show.simplelightbox", function () {});
+
+function openModal(event) {
+  event.preventDefault();
+}
